9f66e3af623ae7b94163e49155ef2f263d583059
PedroCondeOfficial <pedroconde@mail.usf.edu>
2017-12-12 22:09:17 -0500

    New features and read me file made

---
+++ README.md
@@ -0,0 +1,9 @@
+CamSpy
+
+CamSpy is an automation script for the fswebcam program on Linux(install via terminal
+using "sudo apt install fswebcam"). The code prompts you for directory and resolution, and the default 
+file type is JPEG, but can be altered by changing the code.
+
+The primary use is for guarding Linux systems, lawful espionage using a discrete quotidien object,
+and making sure employees are actually doing work. Versions for Windows and Mac are on the way, using different 
+base programs.
--- main.cpp
+++ main.cpp
@@ -8,22 +8,26 @@ using namespace std;
 int main() {
     bool cond = true;
     int n = 1;
+    unsigned int sec;
     char cancel;
-    string directory;
+    string directory, resolution;
 
-    system("apt install fswebcam");
     cout << "Enter directory: ";
     cin >> directory;
+    cout << "Enter resolution(ex: 1920x1080): ";
+    cin >> resolution;
+    cout << "Enter interval in seconds: ";
+    cin >> sec;
 
     while(cond == true){
         std::string a = std::to_string(n);
         std::string filename = "snap" + a;
         cout << filename << endl;
-        std::string command = "fswebcam -r 640x480 --jpeg 85 -D 1 " + directory + filename + ".jpg";
+        std::string command = "fswebcam -r " + resolution + " --jpeg 85 -D 1 " + directory + filename + ".jpg";
         system(command.c_str());
         cout << command << endl;
         n++;
-        sleep(15);
+        sleep(sec);
     }
 
     return 0;
